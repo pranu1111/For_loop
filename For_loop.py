1f02b08b02aac6cff7cdb2eb0d6dbfde1d5b8704
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2023-10-17 10:40:20 +0530

    Add files via upload

---
+++ For_loop.py
@@ -0,0 +1,62 @@
+#for x in "banana":
+#    print(x)
+
+#for character in "Pranita":
+#    print(character)
+
+#a = "pranita"
+#print(a[2])
+#print("k")
+
+#fruits = ["apple" , "banana" , "orange" , "grapes"]
+#for x in fruits:
+#    print(x)
+#    if x == "orange":
+#        break
+
+#fruits = ["apple" ,"orange" , "banana" , "cherry"]
+#for x in fruits:
+#    if x == "orange" or x == "banana":
+#        break
+#    print(x)
+    
+#id = [1,2,3,4,5] 
+#for x in id:
+#    if x == 3 or x == 4:
+#        break
+#    print(x)    
+#    
+#fruits = ["cherry", "banana", "apple", "blueberry"] 
+#for x in fruits:
+#    if x ==  "banana":
+#        break
+#    print(x)
+
+#fruits = ["apple" , "banana", "cherry", "orange"]
+#for x in fruits:
+#    if x == "banana" or x == "cherry":
+#        continue
+#    print(x)
+    
+#for x in range(6):
+#    print(x)
+
+#for x in range(2 , 6):
+#    print(x)
+    
+#for x in range(0,31,5):
+#    print(x)    
+    
+#for x in range(6):
+#    print(x)
+#else:
+#    print("Finaly Finished")
+    
+fruits = ["apple", "cherry" , "orange"]
+adj = ["red" , "big" , "tasty"]
+for x in fruits:
+    for y in adj:
+        print(x,y) 
+        
+for x in [0,2,3]:
+    pass
